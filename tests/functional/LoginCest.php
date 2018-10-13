6d7cc74e37a02616ae5e88484aefedcf31d70098
Pavel Tsymbal <tsymbal.dev@gmail.com>
2018-10-13 17:30:19 +0300

    login functional test

---
+++ tests/functional/LoginCest.php
@@ -0,0 +1,17 @@
+<?php namespace App\Tests;
+
+use App\Tests\FunctionalTester;
+
+class LoginCest
+{
+    public function tryLogin(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Hello guest!', 'h1');
+        $I->click('log in');
+        $I->fillField('_username', 'admin');
+        $I->fillField('_password', '1111');
+        $I->click('Enter');
+        $I->see('Hello! admin', 'h1');
+    }
+}
